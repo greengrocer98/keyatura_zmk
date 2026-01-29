ca9f94264bc4903d83a0e7777f6fd7da41ba34ca
ggrocer <esyunin.maks@mail.ru>
2026-01-29 10:25:18 +0300

    added v0.3 build. Fix bug in led module

--- local_build_v3.sh
+++ local_build_v3.sh
@@ -10,9 +10,9 @@ set -euxo pipefail
 build_mouse () {
     local shield=mouse
     rm -rf $CURRENT_DIR/build/$shield
-    export NRF_MODULE_DIRS="$HOME/zmk-esb/zmk-feature-split-esb/nrf"
-    export NRFXLIB_MODULE_DIRS="$HOME/zmk-esb/zmk-feature-split-esb/nrfxlib"
-    export ZMK_ESB_MODULE_DIRS="$HOME/zmk-esb/zmk-feature-split-esb"
+    export NRF_MODULE_DIRS="$HOME/zmk-esb-v3/zmk-feature-split-esb/nrf"
+    export NRFXLIB_MODULE_DIRS="$HOME/zmk-esb-v3/zmk-feature-split-esb/nrfxlib"
+    export ZMK_ESB_MODULE_DIRS="$HOME/zmk-esb-v3/zmk-feature-split-esb"
     export ZMK_BEHAVIOR_ATTR_CYCLE="$HOME/zmk_modules/zmk-behavior-sensor-attr-cycle"
     export ZMK_PAW_3395_DRIVER="$HOME/zmk_modules/ggrocer-zmk-paw3395-driver"
     # export ZMK_PAW_3395_DRIVER="$HOME/zmk_modules/zmk-paw3395-driver"
@@ -20,7 +20,9 @@ build_mouse () {
     export ZMK_MODULE_DIRS="${ZMK_ESB_MODULE_DIRS};${NRF_MODULE_DIRS};${NRFXLIB_MODULE_DIRS};${ZMK_BEHAVIOR_ATTR_CYCLE};${ZMK_PAW_3395_DRIVER};${ZMK_RGBLED_WIDGET}"
     # export ZMK_MODULE_DIRS="${ZMK_ESB_MODULE_DIRS};${NRF_MODULE_DIRS};${NRFXLIB_MODULE_DIRS};${ZMK_PAW_3395_DRIVER};${ZMK_RGBLED_WIDGET}"
     west build \
-        -p -b lariska \
+        -p -b nice_nano_v2 \
+        -S studio-rpc-usb-uart \
+        -S zmk-usb-logging \
         -d "$CURRENT_DIR/build/$shield" -- \
         -DZMK_CONFIG="$CURRENT_DIR" \
         -DSHIELD=keyatura_$shield \
@@ -33,15 +35,16 @@ build_mouse () {
 build_dongle () {
     local shield=dongle
     rm -rf $CURRENT_DIR/build/$shield
-    export NRF_MODULE_DIRS="$HOME/zmk-esb/zmk-feature-split-esb/nrf"
-    export NRFXLIB_MODULE_DIRS="$HOME/zmk-esb/zmk-feature-split-esb/nrfxlib"
-    export ZMK_ESB_MODULE_DIRS="$HOME/zmk-esb/zmk-feature-split-esb"
+    export NRF_MODULE_DIRS="$HOME/zmk-esb-v3/zmk-feature-split-esb/nrf"
+    export NRFXLIB_MODULE_DIRS="$HOME/zmk-esb-v3/zmk-feature-split-esb/nrfxlib"
+    export ZMK_ESB_MODULE_DIRS="$HOME/zmk-esb-v3/zmk-feature-split-esb"
     export ZMK_BEHAVIOR_ATTR_CYCLE="$HOME/zmk_modules/zmk-behavior-sensor-attr-cycle"
     export ZMK_RGBLED_WIDGET="$HOME/zmk_modules/zmk-vfx-rgbled-indicator"
     export ZMK_MODULE_DIRS="${ZMK_ESB_MODULE_DIRS};${NRF_MODULE_DIRS};${NRFXLIB_MODULE_DIRS};${ZMK_BEHAVIOR_ATTR_CYCLE};${ZMK_RGBLED_WIDGET}"
     west build \
-        -p -b nice_nano \
+        -p -b nice_nano_v2 \
         -S studio-rpc-usb-uart \
+        -S zmk-usb-logging \
         -d "$CURRENT_DIR/build/$shield" -- \
         -DZMK_CONFIG="$CURRENT_DIR" \
         -DSHIELD=keyatura_$shield \
@@ -54,7 +57,7 @@ build_dongle () {
 
 CURRENT_DIR="$(pwd)"
 
-DEFAULTZMKAPPDIR="$HOME/zmk-esb/zmk/"
+DEFAULTZMKAPPDIR="$HOME/zmk-esb-v3/zmk/"
 ZMK_APP_DIR="$DEFAULTZMKAPPDIR/app"
 
 cd $DEFAULTZMKAPPDIR && source .venv/bin/activate && cd -
@@ -65,7 +68,7 @@ export ZEPHYR_SDK_INSTALL_DIR="$HOME/zephyr-sdk-0.17.0"
 
 pushd $ZMK_APP_DIR
 
-build_dongle
+# build_dongle
 build_mouse
 
 deactivate
